07aae742b2f9888d981ae6a469de9c7dd3f2e772
Arjun Vijay Prakash <arjunv.prakash12345@gmail.com>
2024-06-21 17:38:21 +0530

    add header component.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +17,20 @@ export default function RootLayout({
      children: React.ReactNode;
 }>) {
      return (
-          <html lang="en">
-               <body className={inter.className}>{children}</body>
-          </html>
+          <ClerkProvider>
+               <html lang="en">
+                    <body className={`min-h-screen flex flex-col ${inter.className}`}>
+                         {/* <Toaster position="bottom-left" /> */}
+
+                         <header className="border-b sticky top-0 bg-white z-50">
+                              <Header />
+                         </header>
+
+                         <div className=" bg-[#F4F2ED] flex-1 w-full">
+                              <main className="max-w-6xl mx-auto">{children}</main>
+                         </div>
+                    </body>
+               </html>
+          </ClerkProvider>
      );
 }
