c94fe52ae4d431858f9c7cd3470e3e397671f812
Arjun Vijay Prakash <arjunv.prakash12345@gmail.com>
2024-06-22 17:43:53 +0530

    add UserInformation component.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
+import UserInformation from "@/components/UserInformation";
+
 export default function Home() {
      return (
-          <>
-          hello world!
-          </>
+          <section className="grid grid-cols-8 mt-5 sm:px-5">
+               <section className="hidden md:inline md:col-span-2">
+                    <UserInformation />
+               </section>
+
+               <section className="col-span-full md:col-span-6 xl:col-span-4 xl:max-w-xl mx-auto w-full">
+
+               </section>
+
+               <section className="hidden xl:inline justify-center col-span-2">
+                    
+               </section>
+          </section>
      );
 }
---
+++ components/UserInformation.tsx
@@ -0,0 +1,63 @@
+import { currentUser } from "@clerk/nextjs/server";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+// import { IPostDocument } from "@/mongodb/models/post";
+import { Button } from "./ui/button";
+import { SignInButton, SignedIn, SignedOut } from "@clerk/nextjs";
+
+// export default async function UserInformation({ posts }: { posts: IPostDocument[] }) {
+export default async function UserInformation() {
+     const user = await currentUser();
+
+     const firstName = user?.firstName as string;
+     const lastName = user?.lastName as string;
+     const username = user?.username as string;
+     const imageUrl = user?.imageUrl as string;
+
+     // const userPosts = posts?.filter((post) => post.user.userId === user?.id);
+
+     // //  The flatMap() method creates a new array by calling a function for each element in the array and then flattening the result into a new array. It is identical to a map() followed by a flat() of depth 1, but flatMap() is often quite useful, as merging both into one method is slightly more efficient. The result of this flatMap() is a new array that contains all comments made by the current user across all posts. It's "flat" because it's a single-level array, not an array of arrays.
+     // const userComments = posts.flatMap(
+     //      (post) =>
+     //           post?.comments?.filter((comment) => comment.user.userId === user?.id) ||
+     //           []
+     // );
+
+     return (
+          <div className="flex flex-col justify-center items-center bg-white mr-6 rounded-lg border py-4">
+               <Avatar className="h-16 w-16 mb-5">
+                    {user?.id ? <AvatarImage src={imageUrl} /> : <AvatarImage src={`https://avatar.vercel.sh/${username}` || "https://github.com/shadcn.png"} />}
+                    <AvatarFallback>{firstName?.charAt(0)}{lastName?.charAt(0)}</AvatarFallback>
+               </Avatar>
+
+               <SignedIn>
+                    <div className="text-center">
+                         <p className="font-semibold">{firstName} {lastName}</p>
+
+                         <p className="text-xs">@{username}</p>
+                    </div>
+               </SignedIn>
+
+               <SignedOut>
+                    <div className="text-center space-y-2">
+                         <p className="font-semibold">You are not signed in</p>
+
+                         <Button asChild className="bg-[#0B63C4] text-white">
+                              <SignInButton>Sign in</SignInButton>
+                         </Button>
+                    </div>
+               </SignedOut>
+
+               <hr className="w-full border-gray-200 my-5" />
+
+               <div className="flex justify-between w-full px-4 text-sm">
+                    <p className="font-semibold text-gray-400">Posts</p>
+                    <p className="text-blue-400">userPosts.length</p>
+               </div>
+
+               <div className="flex justify-between w-full px-4 text-sm">
+                    <p className="font-semibold text-gray-400">Comments</p>
+                    <p className="text-blue-400">userComments.length</p>
+               </div>
+          </div>
+     );
+}
