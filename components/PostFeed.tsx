24cc176cddbc9db5d908b00304fb06661447e631
Arjun Vijay Prakash <arjunv.prakash12345@gmail.com>
2024-06-29 15:21:17 +0530

    build error fix.

--- components/PostFeed.tsx
+++ components/PostFeed.tsx
@@ -5,7 +5,7 @@ export default async function PostFeed({ posts }: { posts: IPostDocument[] }) {
      return (
           <div className="space-y-2 pb-20">
                {posts?.map((post) => (
-                    <Post key={post._id} post={post} />
+                    <Post key={post._id as string} post={post} />
                ))}
           </div>
      );
